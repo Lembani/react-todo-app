3829f7a8889a0ba6b740edc2c1300bc6c4dbafe0
Lembani Sakala <lembanisakala@gmail.com>
2022-05-30 18:10:09 +0200

    Add `TodoContainer` component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoContainer extends React.PureComponent {
+  
+  render() {
+    return(
+      <h1>Todo App</h1>
+    );
+  }
+}
+
+export default TodoContainer;
