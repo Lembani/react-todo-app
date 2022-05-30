0da75c6eabdc5c4355b6f6a1b720dbfc732f10a9
Lembani Sakala <lembanisakala@gmail.com>
2022-05-30 18:29:58 +0200

    Update Todo Component with state

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import TodoList from './TodoList';
 class TodoContainer extends React.PureComponent {
   
+  state = {
+    todos: [
+      {
+      id: 1,
+      title: "Setup development environment",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Develop website and add content",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Deploy to live server",
+      completed: false
+    }
+    ],
+  }
+
   render() {
     return(
-      <h1>Todo App</h1>
+      <>
+        <h1>Todo App</h1>
+        <TodoList todos={this.state.todos} />
+      </>
     );
   }
 }
