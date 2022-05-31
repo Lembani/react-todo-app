63efa18bac235475c71fe56904af19fb49fad18c
Lembani Sakala <lembanisakala@gmail.com>
2022-05-31 15:47:23 +0200

    Add setState updater

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,37 +1,53 @@
 import React from 'react';
-import Header from './Header';
 
+import Header from './Header';
 import TodoList from './TodoList';
+
 class TodoContainer extends React.PureComponent {
-  
-  state = {
-    todos: [
-      {
-      id: 1,
-      title: "Setup development environment",
-      completed: true
-    },
-    {
-      id: 2,
-      title: "Develop website and add content",
-      completed: false
-    },
-    {
-      id: 3,
-      title: "Deploy to live server",
-      completed: false
-    }
-    ],
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
   }
+
   handleChange = (id) => {
-    console.log('Clicked!', id);
-  }
+    console.log(id);
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
 
   render() {
-    return(
+    const { todos } = this.state;
+    return (
       <>
         <Header />
-        <TodoList todos={this.state.todos} handleChangeProps={() => this.handleChange} />
+        <TodoList todos={todos} handleChangeProps={this.handleChange} />
       </>
     );
   }
