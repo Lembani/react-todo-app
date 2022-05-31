0f196b5a90e1847b8edadd232c6cc82f6bdc0fe9
Lembani Sakala <lembanisakala@gmail.com>
2022-05-31 15:47:47 +0200

    Add props validator

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.PureComponent {
-
   render() {
-    return(
+    const { todo, handleChangeProps } = this.props;
+    return (
       <li>
-        <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
-        {this.props.todo.title}
+        <input type="checkbox" checked={todo.completed} onChange={() => handleChangeProps(todo.id)} />
+        {todo.title}
       </li>
     );
   }
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape(
+    {
+      id: PropTypes.number,
+      completed: PropTypes.bool,
+      title: PropTypes.string,
+    },
+  ).isRequired,
+  handleChangeProps: PropTypes.func.isRequired,
+};
 
 export default TodoItem;
