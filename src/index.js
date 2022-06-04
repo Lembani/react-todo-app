93f85d712fe8386d5b87de6870fd6d8b6bc6f3cd
Lembani Sakala <lembanisakala@gmail.com>
2022-06-04 21:59:45 +0200

    Update styles source

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
-import './index.css';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
