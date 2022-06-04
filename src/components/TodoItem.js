7ebf41f41c1ed3c2d0dc5149dd2d165bdb8985c7
Lembani Sakala <lembanisakala@gmail.com>
2022-06-04 22:01:26 +0200

    Update TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaTrash } from 'react-icons/fa';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.PureComponent {
   render() {
     const { todo, handleChangeProps, deleteTodoProps } = this.props;
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
+          className={styles.checkbox}
           checked={todo.completed}
           onChange={() => handleChangeProps(todo.id)}
         />
+        <button type="button" onClick={() => deleteTodoProps(todo.id)}>
+          <FaTrash style={{ color: 'orangered', fontSize: '16px' }} />
+        </button>
         {todo.title}
-        <button type="button" onClick={() => deleteTodoProps(todo.id)}>Delete</button>
       </li>
     );
   }
