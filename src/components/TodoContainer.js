e4a8646472ae6aeac960f95e41d2a6ac45523621
Lembani Sakala <lembanisakala@gmail.com>
2022-06-04 22:01:07 +0200

    Update TodoContainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,3 +1,7 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/state-in-constructor */
+
 import React from 'react';
 
 import Header from './Header';
@@ -8,24 +12,14 @@ class TodoContainer extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          id: 1,
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: 2,
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((response) => response.json())
+      .then((data) => this.setState({ todos: data }));
   }
 
   handleChange = (id) => {
@@ -51,17 +45,32 @@ class TodoContainer extends React.PureComponent {
     });
   };
 
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
   render() {
     const { todos } = this.state;
     return (
       <>
-        <Header />
-        <InputTodo />
-        <TodoList
-          todos={todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo />
+            <TodoList
+              todos={todos}
+              handleChangeProps={this.handleChange}
+              deleteTodoProps={this.delTodo}
+            />
+          </div>
+        </div>
       </>
     );
   }
